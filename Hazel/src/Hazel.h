bd941b4dddf7db5535a045564c5eee62ca078368
yewen <2492465270@qq.com>
2025-09-21 15:06:27 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// 仅供客户端应用程序包含
+#include<stdio.h>
+#include "Hazel/Application.h"
+
+//----Entry Point-----------------
+#include "Hazel/EntryPoint.h"
+//--------------------------------
